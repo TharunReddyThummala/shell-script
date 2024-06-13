73b4a880b0179ca24bf159c7e7f998d30174fcfa
TharunReddyThummala <tharunreddythummala.fullstack@gmail.com>
2024-06-13 15:34:35 +0530

    checking spe var

--- specialVariables.sh
+++ specialVariables.sh
@@ -8,3 +8,5 @@ echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
+sleep 60 
+echo "process ID of the last bacjground command: $!"
